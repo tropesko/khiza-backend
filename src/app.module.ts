033736548d97b065f97da40babcba127f0c021d6
tropesko <trop.felipe@gmail.com>
2023-07-30 14:35:38 -0300

    setup: starting setup

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
+import { BookmarkModule } from './bookmark/bookmark.module';
+import { UserModule } from './user/user.module';
+import { PrismaService } from './prisma/prisma.service';
+import { PrismaModule } from './prisma/prisma.module';
+
+@Module({
+  imports: [AuthModule, BookmarkModule, UserModule, PrismaModule],
+  providers: [PrismaService],
+})
+export class AppModule {}
+
